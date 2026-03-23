200cc405a0261d999bfe312ad621dc22b001e74d
gary1116 <garygodwin234@gmail.com>
2026-03-23 09:03:50 +0530

    added base files

---
+++ personal_portfolio/src/app/layout.js
@@ -0,0 +1,25 @@
+import { Outfit } from "next/font/google";
+import "./globals.css";
+
+const outfit = Outfit({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+
+
+export const metadata = {
+  title: "Gary Meledath",
+  description: "Portfolio Website",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${outfit.variable}`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
