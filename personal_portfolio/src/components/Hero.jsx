be9abb9303ea2f0aa1e5f8909d2cd9377fdf1b97
gary1116 <garygodwin234@gmail.com>
2026-03-23 15:49:45 +0530

    added icons

--- personal_portfolio/src/components/Hero.jsx
+++ personal_portfolio/src/components/Hero.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import Image from 'next/image'
+import {heroIcons} from '@/assets'
 
 const Hero = () => {
   return (
@@ -20,10 +21,12 @@ const Hero = () => {
             <h1 className='text-center text-3xl font-bold tracking-wider text-gray-500'>My Name is Gary Meledath &</h1>
             <p className='text-lg tracking-wider text-gray-700'>I like animations 😎</p>
         </div>
-        <div className="">
-            <a href="#">Icon</a>
+        <div className="mt-8 flex justify-center gap-x-10 text-3xl text-yellow-600">
+            {heroIcons.map((icon,i)=>(
+                <a href="#" key={i} className='rounded-lg hover:bg-red-400 hover:text-white transition-colors'>{icon}</a>
+            ))}
         </div>
-        <a href="#" className='w-max block rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider mx-auto text-white hover:bg-red-500 transition-colors'>Talk To Me</a>
+        <a href="#" className='w-max mt-7 block rounded-lg bg-red-400 px-3 py-1 font-light capitalize tracking-wider mx-auto text-white hover:bg-red-500 transition-colors'>Talk To Me</a>
       </div>
     </div>
   )
